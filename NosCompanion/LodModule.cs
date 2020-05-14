41a5fe50724d433e03c8cbefe46576519737f2bd
Fernando Souza <fefso1909@gmail.com>
2020-05-14 10:36:13 -0300

    Initial

---
+++ NosCompanion/LodModule.cs
@@ -0,0 +1,39 @@
+﻿using Discord.Commands;
+using System;
+using System.Threading.Tasks;
+
+namespace NosCompanion
+{
+    public class LodModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("lost")]
+        [Summary("Easter egg do steel")]
+        public Task LostAsync()
+            => ReplyAsync("Olá steel, Lost foi um seriado dos anos 2000 que teve seu ultimo episódio em 23 de maio de 2010.");
+
+        [Command("lod")]
+        [Summary("Saiba quando é a proxima lod")]
+        public Task LodAsync()
+        {
+            var state = DateTime.UtcNow.Hour % 3;       
+            
+            switch (state)
+            {
+                case 0:
+                    ReplyAsync($"A lod acabou de terminar, a proxima abre em {60 - DateTime.Now.Minute} minutos.");
+                    break;
+
+                case 1:
+                    var chanel = DateTime.UtcNow.Hour % 8 + 1 % 5;
+                    ReplyAsync($"A lod encontra-se aberta e o DH irá aparecer em {60 - DateTime.Now.Minute} minutos no chanel {chanel}.");
+                    break;
+
+                case 2:
+                    ReplyAsync($"A lod ta rolando com o DH nesse momento, espere a proxima em 1hora e {60 - DateTime.Now.Minute}minutos.");
+                    break;
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ NosCompanion/Program.cs
@@ -0,0 +1,39 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace NosCompanion
+{
+    class Program
+    {
+        private DiscordSocketClient _client;
+        private string _token;
+        private CommandHandler _commandHandler;
+
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult()
+            ;
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commandHandler = new CommandHandler(_client, new Discord.Commands.CommandService());
+            _token = "";
+
+            _client.Log += Log;
+
+            await _client.LoginAsync(TokenType.Bot, _token);
+            await _client.StartAsync();
+           
+            await _commandHandler.InstallCommandsAsync();
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
